3eeabda4c4f24817ac0af1f8bca0c81584f4c3e7
Adrian Sutton <adrian@oplabs.co>
2023-10-09 09:46:42 +1000

    Add script to capture and upload chain data.

---
+++ upload-data.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -euo pipefail
+SOURCE_DIR=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
+
+#GH="op plugin run -- gh"
+GH="gh"
+
+cd "${SOURCE_DIR}/optimism/op-program"
+make capture-chain-test-data
+
+cd "${SOURCE_DIR}"
+RELEASE_NAME=$(git describe --tags)
+echo "Creating release. Note: Job can only run once per tag, to rerun create a new tag or manually delete the existing release."
+$GH release create --latest --verify-tag --title "${RELEASE_NAME}" --notes "Chain test data ${RELEASE_NAME}" "${RELEASE_NAME}"
+
+$GH release upload "${RELEASE_NAME}" ${SOURCE_DIR}/optimism/op-program/compat-tests/*.tar.bz
