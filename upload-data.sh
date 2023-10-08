0cf5a25eda1606a0761b50c5bed34017755a7b4f
Adrian Sutton <adrian@oplabs.co>
2023-10-09 09:49:54 +1000

    Enable test builds

--- upload-data.sh
+++ upload-data.sh
@@ -11,6 +11,6 @@ make capture-chain-test-data
 cd "${SOURCE_DIR}"
 RELEASE_NAME=$(git describe --tags)
 echo "Creating release. Note: Job can only run once per tag, to rerun create a new tag or manually delete the existing release."
-$GH release create --latest --verify-tag --title "${RELEASE_NAME}" --notes "Chain test data ${RELEASE_NAME}" "${RELEASE_NAME}"
+$GH release create --latest --title "${RELEASE_NAME}" --notes "Chain test data ${RELEASE_NAME}" "${RELEASE_NAME}"
 
 $GH release upload "${RELEASE_NAME}" ${SOURCE_DIR}/optimism/op-program/compat-tests/*.tar.bz
