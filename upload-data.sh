dcbe2cf9172d5c6bd80e0a23e5b3f02d02c058ce
Adrian Sutton <adrian@oplabs.co>
2023-10-09 10:38:10 +1000

    Install gh

--- upload-data.sh
+++ upload-data.sh
@@ -2,15 +2,15 @@
 set -euo pipefail
 SOURCE_DIR=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
 
-#GH="op plugin run -- gh"
 GH="gh"
 
-cd "${SOURCE_DIR}/optimism/op-program"
-make capture-chain-test-data
+RELEASE_NAME=$(git describe --tags)
+echo "Logging in to GitHub with access token"
+echo "${GITHUB_ACCESS_TOKEN}" | $GH auth login --with-token
 
 cd "${SOURCE_DIR}"
-RELEASE_NAME=$(git describe --tags)
 echo "Creating release. Note: Job can only run once per tag, to rerun create a new tag or manually delete the existing release."
 $GH release create --latest --title "${RELEASE_NAME}" --notes "Chain test data ${RELEASE_NAME}" "${RELEASE_NAME}"
 
+echo "Uploading chain data to release"
 $GH release upload "${RELEASE_NAME}" ${SOURCE_DIR}/optimism/op-program/compat-tests/*.tar.bz
